3b78bdc6d3f14732cdf769836831f404dafbf807
Ping <qdgaoping@gmail.com>
2021-08-15 21:00:02 +0800

    Update run-Figure9.sh

--- Cases/case_5016/run-Figure9.sh
+++ Cases/case_5016/run-Figure9.sh
@@ -13,17 +13,17 @@
  #LMFF	 	
   bsub -o out.SW5.1CG.LMFF	    -q q_sw_expr -host_stack 256 -share_size 6144 -priv_size 4 -n 1 -b -cgsp 64  ../../LMFF/lmp_sunway_big -in in.replicate
  	
-##G6278C(Intel Xeon Gold 6148)
+##G6278C(Intel Xeon Gold 6278C)
  #Baseline
   mpirun -n 1 ../../Baseline/lmp_intel_cpu_intelmpi -in in.replicate.ilp_tersoff 2>1|tee out.G6278C.1Core.Baseline
  #LMFF
   mpirun -n 1 ../../LMFF/lmp_intel_cpu_intelmpi -in in.replicate 2>1|tee out.G6278C.1Core.LMFF
 
-##G6148(Intel Xeon Gold 6278C)
+##G6148(Intel Xeon Gold 6148)
 #Baseline
-  mpirun -n 1 ../../Baseline/lmp_intel_cpu_intelmpi -in in.replicate.ilp_tersoff 2>1|tee out.G6278C.1Core.Baseline
+  mpirun -n 1 ../../Baseline/lmp_intel_cpu_intelmpi -in in.replicate.ilp_tersoff 2>1|tee out.G6148.1Core.Baseline
  #LMFF
-  mpirun -n 1 ../../LMFF/lmp_intel_cpu_intelmpi -in in.replicate 2>1|tee out.G6278C.1Core.LMFF
+  mpirun -n 1 ../../LMFF/lmp_intel_cpu_intelmpi -in in.replicate 2>1|tee out.G6148.1Core.LMFF
 
 ##E5(Intel Xeon E5 2680-v3 on Sunway Supercomputer)
 #Baseline
